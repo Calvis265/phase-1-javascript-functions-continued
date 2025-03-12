725f2c1a9fc84e6302440b7d5db5c74634e7d09d
calvis onyango <calvisonyango265@gmail.com>
2025-03-12 17:33:44 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// Defines the saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Defines the mondayWork function
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+// Defines the wrapAdjective function
+function wrapAdjective(wrapper = "*") {
+    return function(adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
+
+ 
